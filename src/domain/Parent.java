9afd04e609eba75fc15f34fab8ac7960699a2e6b
DESKTOP-414TTEI\HP <1614405845@qq.com>
2024-05-21 17:27:07 +0800

    删一个不要的类

--- src/domain/Parent.java
+++
@@ -1,9 +0,0 @@
-package domain;
-
-public class Parent extends User{
-    User child;
-    public Parent(String username, String password, String identity) {
-        super(username, password, identity);
-    }
-
-}
