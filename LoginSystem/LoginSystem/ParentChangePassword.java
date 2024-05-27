2ff9a811c82229c88a3fd0ccf811e85170c22cfc
BigStrongSun <sundazhuang@hotmail.com>
2024-05-27 17:21:03 +0800

    测试

---
+++ LoginSystem/LoginSystem/ParentChangePassword.java
@@ -0,0 +1,142 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+
+public class ParentChangePassword {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Change Password");
+        frame.setSize(400, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel panel = new JPanel();
+        frame.add(panel);
+
+        placeComponents(panel);
+
+        frame.setVisible(true);
+    }
+
+    private static void placeComponents(JPanel panel) {
+
+        panel.setLayout(null);
+
+        JLabel userLabel = new JLabel("Username:");
+        userLabel.setBounds(10, 20, 80, 25);
+        panel.add(userLabel);
+
+        JTextField userText = new JTextField(20);
+        userText.setBounds(100, 20, 165, 25);
+        panel.add(userText);
+
+        JLabel passwordLabel = new JLabel("Old Password:");
+        passwordLabel.setBounds(10, 50, 80, 25);
+        panel.add(passwordLabel);
+
+        JPasswordField passwordText = new JPasswordField(20);
+        passwordText.setBounds(100, 50, 165, 25);
+        panel.add(passwordText);
+
+        JLabel newPasswordLabel = new JLabel("New Password:");
+        newPasswordLabel.setBounds(10, 80, 80, 25);
+        panel.add(newPasswordLabel);
+
+        JPasswordField newPasswordText = new JPasswordField(20);
+        newPasswordText.setBounds(100, 80, 165, 25);
+        panel.add(newPasswordText);
+
+        JButton changePasswordButton = new JButton("Change Password");
+        changePasswordButton.setBounds(10, 110, 165, 25);
+        panel.add(changePasswordButton);
+
+        changePasswordButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String username = userText.getText();
+                String oldPassword = new String(passwordText.getPassword());
+                String newPassword = new String(newPasswordText.getPassword());
+
+                if (validatePassword(username, oldPassword)) {
+                    changeUserPassword(username, newPassword);
+                    JOptionPane.showMessageDialog(null, "Password changed successfully");
+                } else {
+                    JOptionPane.showMessageDialog(null, "Invalid old password");
+                }
+            }
+        });
+    }
+
+    private static boolean validatePassword(String username, String password) {
+    	File file = new File("parent.txt");
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            String line = reader.readLine();
+            while (line != null) {
+                String[] user = line.split(":");
+                if (user[0].equals(username)) {
+                    return user[1].equals(password);
+                }
+                line = reader.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return false;
+    }
+
+    private static void changeUserPassword(String username, String newPassword) {
+    	File file = new File("parent.txt");
+        BufferedReader reader = null;
+        File tempFile = new File("temp0.txt");
+        BufferedWriter writer = null;
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            writer = new BufferedWriter(new FileWriter(tempFile));
+            String line = reader.readLine();
+            while (line != null) {
+                String[] user = line.split(":");
+                if (user[0].equals(username)) {
+                    writer.write(username + ":" + newPassword);
+                } else {
+                    writer.write(line);
+                }
+                writer.newLine();
+                line = reader.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+                if (writer != null) {
+                    writer.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        file.delete();
+        tempFile.renameTo(file);
+    
+        System.out.println("Password for user " + username + " changed to " + newPassword);
+    }
+}
--- src/util/IOController.java
+++ src/util/IOController.java
@@ -1,116 +1,134 @@
 package util;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.PrintWriter;
+import java.io.*;
+
 /**
- *  The util.IOController class provides some simple methods for file input or output.
- *  Title      : util.IOController.java
- *  <p>
- *  Description:
- *  <p>
- *  The util.IOController class provides some simple methods for file input or output.
- *  <p>
- *  The most important feature of this class is that it can switch input and output flexibly
- *  and it has complete exception handling.
- *  @author  Tianyi Liu
- *  @version 1.1
+ * The util.IOController class provides some simple methods for file input or output.
+ * Title      : util.IOController.java
+ * <p>
+ * Description:
+ * <p>
+ * The util.IOController class provides some simple methods for file input or output.
+ * <p>
+ * The most important feature of this class is that it can switch input and output flexibly
+ * and it has complete exception handling.
+ * </p>
+ * @version 1.2
  */
 public class IOController {
     private String filename;
     private File file;
+    private boolean isResource;
 
-    /** Constructor of util.IOController.
+    /**
+     * Constructor of util.IOController.
      * @param filename the name of the file which needs to be I/O.
      */
     public IOController(String filename) {
         this.filename = filename;
         file = new File(filename);
+        if (!file.exists()) {
+            // Check if the file exists as a resource in JAR
+            InputStream inputStream = getClass().getClassLoader().getResourceAsStream(filename);
+            if (inputStream != null) {
+                isResource = true;
+            }
+        }
     }
 
-    private boolean checkExist() {
+    /**
+     * Check if the file exists.
+     * @return true if the file exists, false otherwise.
+     */
+    public boolean fileExists() {
+        if (isResource) {
+            return getClass().getClassLoader().getResource(filename) != null;
+        }
         return file.exists();
     }
 
-    /** write data to the file.
-     * @param data the data needed to be write
-     * @return Whether it was written successfully or not
+    /**
+     * Get the absolute path of the file.
+     * @return the absolute path of the file.
+     */
+    public String getFilePath() {
+        if (isResource) {
+            return getClass().getClassLoader().getResource(filename).getPath();
+        }
+        return file.getAbsolutePath();
+    }
+
+    /**
+     * Write data to the file.
+     * @param data the data needed to be written.
+     * @return Whether it was written successfully or not.
      */
     public boolean directWrite(String data) {
-        OutputStream os;
-        try {
-            os = new FileOutputStream(filename);
-            PrintWriter pw = new PrintWriter(os);
+        if (isResource) {
+            throw new UnsupportedOperationException("Cannot write to a resource file.");
+        }
+        try (OutputStream os = new FileOutputStream(file);
+             PrintWriter pw = new PrintWriter(os)) {
             pw.print(data);
-            pw.close();
-            os.close();
+            return true;
         } catch (Exception e) {
             e.printStackTrace();
             return false;
         }
-        return true;
     }
-
-    /** read data from the file.
-     * @return the data which are read from the file
-     */
-    public String directRead() {
-        if(!checkExist()) {
-            return null;
-        }
-        int iAvail;
-        String ret=null;
-        try {
-            InputStream is = new FileInputStream(filename);
-            iAvail = is.available();
-            byte[] bytes = new byte[iAvail];
-            is.read(bytes);
-            is.close();
-            ret = new String(bytes, "UTF-8");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return ret;
-    }
-
 
     /**
-     * get the file name.
-     * @return the filename
+     * Read data from the file.
+     * @return the data which are read from the file.
+     */
+    public String directRead() {
+        if (!fileExists()) {
+            return null;
+        }
+        try (InputStream is = isResource ? getClass().getClassLoader().getResourceAsStream(filename) : new FileInputStream(file);
+             ByteArrayOutputStream buffer = new ByteArrayOutputStream()) {
+            byte[] data = new byte[1024];
+            int nRead;
+            while ((nRead = is.read(data, 0, data.length)) != -1) {
+                buffer.write(data, 0, nRead);
+            }
+            buffer.flush();
+            return new String(buffer.toByteArray(), "UTF-8");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * Get the file name.
+     * @return the filename.
      */
     public String getFilename() {
         return filename;
     }
 
-
     /**
-     * set the file name.
-     * @param filename the filename to set
+     * Set the file name.
+     * @param filename the filename to set.
      */
     public void setFilename(String filename) {
         this.filename = filename;
     }
 
-
     /**
-     * get the file object.
-     * @return the file
+     * Get the file object.
+     * @return the file.
      */
     public File getFile() {
         return file;
     }
 
-
     /**
-     * set the file object
-     * @param file the file to set
+     * Set the file object.
+     * @param file the file to set.
      */
     public void setFile(File file) {
         this.file = file;
     }
-
 }
